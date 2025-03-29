6fd28f0bf125606d8519231ec4dcc3f1194b05e4
Adi Levi <adile0194@gmail.com>
2025-03-29 14:40:26 +0300

    feat: added use effect to get sub dendrogram from server

--- app/src/pages/Home/HomePage.jsx
+++ app/src/pages/Home/HomePage.jsx
@@ -2,20 +2,25 @@ import React, { useEffect, useState } from 'react';
 import Dendrogram from '../../components/Dendrogram/Dendrogram'
 // import data from "../../data/dendrogramMock.json";
 import SubDendrogramForm from '../../components/SubDendrogramForm/SubDendrogramForm';
-import {fetchSubDendrogram} from '../../services/dendrogram.service';
+import { fetchSubDendrogram } from '../../services/dendrogram.service';
 
 const HomePage = () => {
   const [subDengrogram, setSubDendrogram] = useState(null);
 
   useEffect(() => {
-    const url = window.location.href;
-    
-
-    async function getSubDendrogram() {
-      const result = await fetchSubDendrogram();
-      setCampaign(result.data);
+    async function getSubDendrogram(data) {
+      const result = await fetchSubDendrogram(data);
+      setSubDendrogram(result.data);
     }
 
+    // Mock data for testing
+    const data = {
+      "dataset": "imagenet",
+      "selected_labels": ["Persian_cat", "tabby", "orange", "lemon", "zucchini", "broccoli", "teapot", "coffeepot", "warplane", "space_shuttle", "American_coot", "black_swan"],
+      "z_filename": "dendrogram_similarity_mini_imagenet"
+    }
+
+    getSubDendrogram(data);
   }, []);
 
 
@@ -26,7 +31,11 @@ const HomePage = () => {
         <SubDendrogramForm />
       </aside>
       <main>
-        <Dendrogram data={data} />
+        {subDengrogram ? (
+          <Dendrogram data={subDengrogram} />
+        ) : (
+          <div>Please upload a model</div>
+        )}
       </main>
     </>
   );
