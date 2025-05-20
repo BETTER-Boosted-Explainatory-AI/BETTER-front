dad71ccd1d9a60f5adf9e36a26f04bcb78fc81a3
MoranSin <nurixhbh@gmail.com>
2025-05-20 19:22:35 +0300

    feat: adversarial detection and analysis menu

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes, useLocation } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { ThemeProvider } from "@mui/material/styles";
 import "./App.css";
 import BetterTheme from "./MuiTheme.style.js";
@@ -7,45 +7,14 @@ import { DendrogramProvider } from "./contexts/DendrogramProvider";
 import Header from "./components/Header/Header";
 import HomePage from "./pages/Home/HomePage";
 import WhiteboxTestingPage from "./pages/WhiteBoxTesting/WhiteBoxTestingPage";
-import AdversarialAttacksPage from "./pages/AdversarialAttacks/AdversarialAttacksPage";
+import AdversarialDetectionPage from "./pages/AdversarialDetection/AdversarialDetectionPage.jsx";
+import AdversarialAnalysisPage from "./pages/AdversarialAnalysis/AdversarialAnalysisPage.jsx";
 import QueryPage from "./pages/Query/QueryPage";
 import LoginPage from "./pages/Login/LoginPage.jsx";
 import NotFoundPage from "./pages/NotFound/NotFoundPage.jsx";
 import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
 function App() {
 
-  // const location = useLocation();
-  // const hideHeader = location.pathname === "/Login";
-
-  // return (
-  //   <ThemeProvider theme={BetterTheme}>
-  //     {!hideHeader && (
-  //       <header>
-  //         <Header />
-  //       </header>
-  //     )}
-  //     <div id="wrapper">
-  //       <ModelProvider>
-  //         <DendrogramProvider>
-  //           <Routes>
-  //             <Route path="/" element={<HomePage />} />
-  //             <Route path="/Login" element={<LoginPage />} />
-  //             <Route path="/Query" element={<QueryPage />} />
-  //             <Route
-  //               path="/WhiteboxTesting"
-  //               element={<WhiteboxTestingPage />}
-  //             />
-  //             <Route
-  //               path="AdversarialAttacks"
-  //               element={<AdversarialAttacksPage />}
-  //             />
-  //               <Route path="*" element={<NotFoundPage />} />
-  //           </Routes>
-  //         </DendrogramProvider>
-  //       </ModelProvider>
-  //     </div>
-  //   </ThemeProvider>
-  // );
   return (
     <ThemeProvider theme={BetterTheme}>
         <header>
@@ -81,10 +50,18 @@ function App() {
                 }
               />
                <Route
-                path="AdversarialAttacks"
+                path="Adversarial/Detection"
                 element={
                   <ProtectedRoute>
-                    <AdversarialAttacksPage />
+                    <AdversarialDetectionPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="Adversarial/Analysis"
+                element={
+                  <ProtectedRoute>
+                    <AdversarialAnalysisPage />
                   </ProtectedRoute>
                 }
               />
