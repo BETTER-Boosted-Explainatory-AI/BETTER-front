e079a7e7200220977124baac138a22cbd58aafb3
Adi Levi <104142857+adi-git33@users.noreply.github.com>
2025-05-30 19:10:01 +0300

    fix: fixed font size for navigation

--- app/src/components/NavBar/NavBar.jsx
+++ app/src/components/NavBar/NavBar.jsx
@@ -33,7 +33,6 @@ const Navbar = () => {
     setValue(getTabValue(location.pathname));
   }, [location.pathname]);
 
-
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
@@ -50,54 +49,54 @@ const Navbar = () => {
         textColor="secondary"
         indicatorColor="secondary"
         aria-label="navigation tabs"
-        gap={10}
+        gap={20}
       >
         <Tab
-          sx={{ mx: 2 }}
+          sx={{ mx: 5, fontSize: "1rem" }}
           label="Query"
           value={ROUTES.QUERY}
           component={Link}
           to={ROUTES.QUERY}
         />
         <Tab
-          sx={{ mx: 2 }}
+          sx={{ mx: 5, fontSize: "1rem" }}
           label="White-box Testing"
           value={ROUTES.WHITEBOX_TESTING}
           component={Link}
           to={ROUTES.WHITEBOX_TESTING}
         />
         <Tab
-          sx={{ mx: 2 }}
+          sx={{ mx: 5, fontSize: "1rem" }}
           label="Adversarial Attacks"
           value={ROUTES.ADVERSARIAL_ATTACKS}
           onClick={(e) => {
             e.preventDefault();
-              setMenuAnchor(e.currentTarget);
+            setMenuAnchor(e.currentTarget);
           }}
         />
       </Tabs>
-        <Menu
-          anchorEl={menuAnchor}
-          open={Boolean(menuAnchor)}
-          onClose={handleMenuClose}
-        >
-          <MenuItem>
-            <Tab
-              label="Attack Detection"
-              value={ROUTES.ADVERSARIAL_DETECTION}
-              component={Link}
-              to={ROUTES.ADVERSARIAL_DETECTION}
-            />
-          </MenuItem>
-          <MenuItem>
-            <Tab
-              label="Attack Demonstration"
-              value={ROUTES.ADVERSARIAL_ANALYSIS}
-              component={Link}
-              to={ROUTES.ADVERSARIAL_ANALYSIS}
-            />
-          </MenuItem>
-        </Menu>
+      <Menu
+        anchorEl={menuAnchor}
+        open={Boolean(menuAnchor)}
+        onClose={handleMenuClose}
+      >
+        <MenuItem>
+          <Tab
+            label="Attack Detection"
+            value={ROUTES.ADVERSARIAL_DETECTION}
+            component={Link}
+            to={ROUTES.ADVERSARIAL_DETECTION}
+          />
+        </MenuItem>
+        <MenuItem>
+          <Tab
+            label="Attack Demonstration"
+            value={ROUTES.ADVERSARIAL_ANALYSIS}
+            component={Link}
+            to={ROUTES.ADVERSARIAL_ANALYSIS}
+          />
+        </MenuItem>
+      </Menu>
     </NavbarContainer>
   );
 };
