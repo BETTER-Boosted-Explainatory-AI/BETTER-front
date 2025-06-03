08e6a7396bbcbaca5e61d9377bcf16bf56ebb8c2
MoranSin <nurixhbh@gmail.com>
2025-06-03 19:39:49 +0300

    feat: little animation for selecting card

--- app/src/components/ClickableCard/ClickableCard.style.js
+++ app/src/components/ClickableCard/ClickableCard.style.js
@@ -8,9 +8,13 @@ export const ClickableCardStyle = styled(Card)(({ theme, selected }) => ({
     color: theme.palette.primary.main,
     fontWeight: selected ? '600' : '400',
     cursor: 'pointer',
+    transition: 'transform 0.25s cubic-bezier(.4,2,.6,1), background-color 0.2s',
     '&:hover': {
         backgroundColor: theme.palette.customColors.paleBlue,
         color: theme.palette.primary.main,
+    },
+    '&:active': {
+        transform: 'scale(1.1)',
     },
     padding: '0.2em 0.3em 0em 0.3em',
     borderRadius: '10px',
